028274518e58fa529e0e375b31673c259e8513e9
CuonoMaione <cuonomaione99@outlook.it>
2023-06-15 18:07:20 +0200

    HelloWord!

---
+++ hello-word/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+    name: 'App',
+    components:{
+      AppTitle,
+    }
+}
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+</template>
+
+<style>
+</style>
